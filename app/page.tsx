d460517afa3c524d94cf55364e2e0c503343cc0b
Nick Melms <nmelms92@gmail.com>
2025-12-10 08:39:24 -0800

    image uplaod

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
 
             <div className="relative aspect-square rounded-lg overflow-hidden border border-border">
               <Image
-                src="/modern-geometric-patterns.jpg"
+                src="https://plus.unsplash.com/premium_photo-1764546983241-9e33d0c26893?q=80&w=1620&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Optimized placeholder image 2"
                 fill
                 className="object-cover"
